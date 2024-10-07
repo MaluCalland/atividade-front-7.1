2639d6b00a630d4532c3ffc94e7fa31ad79267f0
MaluCalland <malucalland@gmail.com>
2024-10-07 10:12:10 -0300

    Adicinando Pastas da Atividade 7 - Parte 1

---
+++ pratica-7.1/src/Components/Login/Login.jsx
@@ -0,0 +1,62 @@
+import {FaUser, FaLock} from 'react-icons/fa';
+
+import { useState } from 'react';
+
+import './Login.css';
+
+const Login = () => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        alert("enviando os dados: " + username + " - " + password);
+
+        // console.log("teste", username, password)
+        // console.log("Envio");
+    }
+
+  return (
+    <div className="container">
+        <form onSubmit={handleSubmit}>
+            <h1>Acesse o sistema</h1>
+            <div className='input-field'>
+                <input 
+                    type="email" 
+                    placeholder='E-mail'
+                    required
+                    onChange={(e) => setUsername(e.target.value)}
+                />
+                <FaUser className='icon'/>
+            </div>
+            <div className='input-field'>
+                <input 
+                    type="password"
+                    placeholder='Senha'
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+               <FaLock className='icon'/>
+            </div>
+
+            <div className="recall-forget">
+                <label htmlFor="">
+                    <input type="checkbox" />
+                    Lembrar de mim
+                </label>
+                <a href='#'>Esqueceu a senha?</a>
+            </div>
+            
+            <button>Entrar</button>
+
+            <div className="signup-link">
+                <p>
+                    Não tem uma conta? <a href='#'>Registrar</a>
+                </p>
+            </div>
+        </form>
+    </div>
+  )
+}
+
+export default Login
